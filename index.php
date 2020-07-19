fe9cd273b125c6e13d53d5388f1e311810f8061e
mohssineAboutaj <mohssine@mohssine-HP-Compaq-6200.(none)>
2020-07-19 14:30:25 +0100

    styling

--- index.php
+++ index.php
@@ -1,41 +1,53 @@
-</html>
+<?php
+
+require_once( './inc/connect.php' );
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang = 'en'>
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-<link rel="stylesheet" href="template/style.css">
-<link href="css/style.css" rel="stylesheet">
+<meta charset = 'UTF-8'>
+<meta name = 'viewport' content = 'width=device-width, initial-scale=1.0'>
+<meta http-equiv = 'X-UA-Compatible' content = 'ie=edge'>
+<meta name = 'Description' content = 'Enter your description here'/>
+<link rel = 'stylesheet' href = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css'>
+<link rel = 'stylesheet' href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css'>
+<link rel = 'stylesheet' href = 'template/style.css'>
+<link href = 'css/style.css' rel = 'stylesheet'>
 <title>login-panal</title>
-
 </head>
-<body>
+<body style = 'background-image: url(./template/img/back.jpg)'>
 
-<form class="form-login" action="">
+<div class = 'row'>
+<div class = 'col-sm-12 col-md-4 form-container'>
+<form class = 'form-login' action = ''>
+<div class = 'form-group'>
+<h3>login</h3>
+</div>
+<div class = 'form-group'>
+<label for = 'username'><span class = 'label label-default'>USERNAME</span></label>
+<input type = 'text' class = 'form-control' name = 'username' id = 'username' aria-describedby = 'helpUsername' placeholder = 'username'>
+<small id = 'helpUsername' class = 'form-text text-muted'>Help text</small>
+</div>
+<div class = 'form-group'>
+<label  for = 'password'><span class = 'label label-default'>PASSWORD</span></label>
+<input type = 'password'
 
-<div class="form-group">
-  <label for="username"><span class="label label-default">USERNAME</span></label>
-  <input type="text"
-    class="form-control" name="username" id="username" aria-describedby="helpUsername" placeholder="username">
-  <small id="helpUsername" class="form-text text-muted">Help text</small>
+class = 'form-control' name = 'password' id = 'password' aria-describedby = 'helpPassword' placeholder = '********'>
+<small id = 'helpPassword' class = 'form-text text-muted'>Help text</small>
 </div>
-<div class="form-group">
-  <label  for="password"><span class="label label-default">PASSWORD</span></label>
-  <input type="password"
-    class="form-control" name="password" id="password" aria-describedby="helpPassword" placeholder="********">
-  <small id="helpPassword" class="form-text text-muted">Help text</small>
-</div>
-<div class="form-group">
-  <input class="btn btn-success btn-block" value="Login" type="submit">
+<div class = 'form-group'>
+<input class = 'btn btn-success btn-block' value = 'Login' type = 'submit'>
 </div>
 </form>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</div>
+</div>
+
+<script src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js'></script>
+<script src = 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js'></script>
+<script src = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js'></script>
+
 </body>
 </html>
--- registration.php
+++ registration.php
@@ -1,4 +1,3 @@
-</html>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,39 +12,48 @@
 <title>login-panal</title>
 
 </head>
-<body>
+<body style="background-image: url(./template/img/back.jpg)">
 
-<form class="form-reg" action="">
+<div class="row">
+	<div class="col-sm-12 col-md-4 form-container">
 
-<div class="form-group">
-  <label for="name"><span class="label label-default">USERNAME</span></label>
-  <input type="text"
-    class="form-control" name="name" id="name" aria-describedby="helpNname" placeholder="name">
-  <small id="helpName" class="form-text text-muted">Help text</small>
-</div>
-<div class="form-group">
-  <label for="mail"><span class="label label-default">Email</span></label>
-  <input type="text"
-    class="form-control" name="mail" id="mail" aria-describedby="helpMail" placeholder="example@example.fr">
-  <small id="helpMail" class="form-text text-muted">Help text</small>
-</div>
-<div class="form-group">
-  <label  for="pass1"><span class="label label-default">pass1</span></label>
-  <input type="pass1"
-    class="form-control" name="pass1" id="pass1" aria-describedby="helpPass1" placeholder="********">
-  <small id="helpPass1" class="form-text text-muted">Help text</small>
-</div>
+		<form class="form-reg" action="">
+			<div class="form-group">
+				<h3>register</h3>
+			</div>
 
-<label  for="pass2"><span class="label label-default">pass1</span></label>
-  <input type="pass2"
-    class="form-control" name="pass2" id="pass2" aria-describedby="helpPass2" placeholder="********">
-  <small id="helpPass2" class="form-text text-muted">Help text</small>
-</div>
+			<div class="form-group">
+				<label for="name"><span class="label label-default">USERNAME</span></label>
+				<input type="text"
+					class="form-control" name="name" id="name" aria-describedby="helpNname" placeholder="name">
+				<small id="helpName" class="form-text text-muted">Help text</small>
+			</div>
+			<div class="form-group">
+				<label for="mail"><span class="label label-default">Email</span></label>
+				<input type="text"
+					class="form-control" name="mail" id="mail" aria-describedby="helpMail" placeholder="example@example.fr">
+				<small id="helpMail" class="form-text text-muted">Help text</small>
+			</div>
+			<div class="form-group">
+				<label  for="pass1"><span class="label label-default">pass1</span></label>
+				<input type="pass1"
+					class="form-control" name="pass1" id="pass1" aria-describedby="helpPass1" placeholder="********">
+				<small id="helpPass1" class="form-text text-muted">Help text</small>
+			</div>
 
-<div class="form-group">
-  <input class="btn btn-success btn-block" value="Registration" type="submit">
+			<div class="form-group">
+				<label  for="pass2"><span class="label label-default">pass1</span></label>
+					<input type="pass2"
+						class="form-control" name="pass2" id="pass2" aria-describedby="helpPass2" placeholder="********">
+				<small id="helpPass2" class="form-text text-muted">Help text</small>
+			</div>
+
+			<div class="form-group">
+				<input class="btn btn-success btn-block" value="Registration" type="submit">
+			</div>
+		</form>
+	</div>
 </div>
-</form>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
